c04caa0720819d5a11f2f85635059d50e4050ae7
Shiva Vishwakarma <63403914+ShivaVkm@users.noreply.github.com>
2021-07-19 15:27:18 +0530

    Update ticTacToe.py

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,34 +1,32 @@
 
-board = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-"]
+board = ["-", "-", "-","-", "-", "-","-", "-", "-"]
 
-game_still_going = True
+gameStillGoing = True
 
 winner = None
 
-current_player = "X"
+currentPlayer = "X"
 
 def play_game():
 
-  display_board()
-  while game_still_going:
-    handle_turn(current_player)
-    check_if_game_over()
-    flip_player()
+  displayBoard()
+  while gameStillGoing:
+    handleTurn(current_player)
+    checkIfGameOver()
+    flipPlayer()
   if winner == "X" or winner == "O":
     print(winner + " won.")
   elif winner == None:
     print("Tie.")
 
-def display_board():
+def displayBoard():
   print("\n")
   print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
   print(board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
   print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
   print("\n")
 
-def handle_turn(player):
+def handleTurn(player):
   print(player + "'s turn.")
   position = input("Choose a position from 1-9: ")
   valid = False
@@ -41,86 +39,86 @@ def handle_turn(player):
     else:
       print("You can't go there. Go again.")
   board[position] = player
-  display_board()
+  displayBoard()
 
-def check_if_game_over():
-  check_for_winner()
-  check_for_tie()
+def checkIfGameOver():
+  checkForWinner()
+  checkForTie()
 
-def check_for_winner():
+def checkForWinner():
   global winner
-  row_winner = check_rows()
-  column_winner = check_columns()
-  diagonal_winner = check_diagonals()
-  if row_winner:
-    winner = row_winner
-  elif column_winner:
-    winner = column_winner
-  elif diagonal_winner:
-    winner = diagonal_winner
+  rowWinner = checkRows()
+  columnWinner = checkColumns()
+  diagonalWinner = checkDiagonals()
+  if rowWinner:
+    winner = rowWinner
+  elif columnWinner:
+    winner = columnWinner
+  elif diagonalWinner:
+    winner = diagonalWinner
   else:
     winner = None
 
-def check_rows():
-  global game_still_going
+def checkRows():
+  global gameStillGoing
   row_1 = board[0] == board[1] == board[2] != "-"
   row_2 = board[3] == board[4] == board[5] != "-"
   row_3 = board[6] == board[7] == board[8] != "-"
-  if row_1 or row_2 or row_3:
-    game_still_going = False
-  if row_1:
+  if row1 or row2 or row3:
+    gameStillGoing = False
+  if row1:
     return board[0] 
-  elif row_2:
+  elif row2:
     return board[3] 
-  elif row_3:
+  elif row3:
     return board[6] 
   else:
     return None
 
-def check_columns():
-  global game_still_going
-  column_1 = board[0] == board[3] == board[6] != "-"
-  column_2 = board[1] == board[4] == board[7] != "-"
-  column_3 = board[2] == board[5] == board[8] != "-"
-  if column_1 or column_2 or column_3:
-    game_still_going = False
-  if column_1:
+def checkColumns():
+  global gameStillGoing
+  column1 = board[0] == board[3] == board[6] != "-"
+  column2 = board[1] == board[4] == board[7] != "-"
+  column3 = board[2] == board[5] == board[8] != "-"
+  if column1 or column2 or column3:
+    gameStillGoing = False
+  if column1:
     return board[0] 
-  elif column_2:
+  elif column2:
     return board[1] 
-  elif column_3:
+  elif column3:
     return board[2] 
   else:
     return None
 
-def check_diagonals():
-  global game_still_going
-  diagonal_1 = board[0] == board[4] == board[8] != "-"
-  diagonal_2 = board[2] == board[4] == board[6] != "-"
-  if diagonal_1 or diagonal_2:
-    game_still_going = False
-  if diagonal_1:
+def checkDiagonals():
+  global gameStillGoing
+  diagonal1 = board[0] == board[4] == board[8] != "-"
+  diagonal2 = board[2] == board[4] == board[6] != "-"
+  if diagonal1 or diagonal2:
+    gameStillGoing = False
+  if diagonal1:
     return board[0] 
-  elif diagonal_2:
+  elif diagonal2:
     return board[2]
   else:
     return None
 
 
-def check_for_tie():
-  global game_still_going
+def checkForTie():
+  global gameStillGoing
   if "-" not in board:
-    game_still_going = False
+    gameStillGoing = False
     return True
   else:
     return False
 
 
-def flip_player():
-  global current_player
-  if current_player == "X":
-    current_player = "O"
-  elif current_player == "O":
-    current_player = "X"
+def flipPlayer():
+  global currentPlayer
+  if currentPlayer == "X":
+    currentPlayer = "O"
+  elif currentPlayer == "O":
+    currentPlayer = "X"
 
-play_game()
+playGame()
